363124d14e9c499ffcebeb551fc1172728def47b
장진형 <jhyoung75@gmail.com>
2026-02-14 16:40:12 +0900

    Add sample.tar.gz

--- examples/test.ts
+++ examples/test.ts
@@ -8,6 +8,9 @@ fs.readFile(filePath, "utf-8", (err, data) => {
 		console.error("Error reading file:", err);
 		return;
 	}
+	if (data) {
+		console.log("==> Test Data:", data);
+	}
 });
 
 console.log("File read successfully");
